ec3d7c8cdd1fc4c8da81614f7b16c0cdcd573865
David Siu <davidsiu@gmail.com>
2013-11-22 16:26:30 -0800

    Making grid NOT take over pointer events. Removing google analytics

--- gd-bookmarklet.js
+++ gd-bookmarklet.js
@@ -151,7 +151,7 @@
                     + "  <div class=\"gdt-button\"><a href=\"#\" id=\"gdt-close\">Close</a></div>"
                     + "</div>";
       
-      $("head").append("<link rel='stylesheet' type='text/css' href='http://alefeuvre.github.com/foundation-grid-displayer/stylesheets/gd-bookmarklet.min.css'>");
+      $("head").append("<link rel='stylesheet' type='text/css' href='http://github.com/davidsiu/foundation-grid-displayer/stylesheets/gd-bookmarklet.min.css'>");
       $("body").prepend(gridHtml).prepend(gridToolsHtml);  
       $("#grid-displayer-tools").delay(1200).fadeTo("slow",0.1); 
       
@@ -203,39 +203,4 @@
     startBookmarklet(window.jQuery);
   }
   
-  /**
-   * Google Analytics JS v1
-   * http://code.google.com/p/google-analytics-js/
-   * Copyright (c) 2009 Remy Sharp remysharp.com / MIT License
-   * $Date: 2009-02-25 14:25:01 +0000 (Wed, 25 Feb 2009) $
-   */
-  (function(urchinCode, domain, url) {
-      
-    function rand(min, max) {
-        return min + Math.floor(Math.random() * (max - min));
-    }
-      
-    var i=1000000000,
-        utmn=rand(i,9999999999), //random request number
-        cookie=rand(10000000,99999999), //random cookie number
-        random=rand(i,2147483647), //number under 2147483647
-        today=(new Date()).getTime(),
-        win = window.location,
-        img = new Image(),
-        urchinUrl = 'http://www.google-analytics.com/__utm.gif?utmwv=1.3&utmn='
-            +utmn+'&utmsr=-&utmsc=-&utmul=-&utmje=0&utmfl=-&utmdt=-&utmhn='
-            +domain+'&utmr='+win+'&utmp='
-            +url+'&utmac='
-            +urchinCode+'&utmcc=__utma%3D'
-            +cookie+'.'+random+'.'+today+'.'+today+'.'
-            +today+'.2%3B%2B__utmb%3D'
-            +cookie+'%3B%2B__utmc%3D'
-            +cookie+'%3B%2B__utmz%3D'
-            +cookie+'.'+today
-            +'.2.2.utmccn%3D(referral)%7Cutmcsr%3D' + win.host + '%7Cutmcct%3D' + win.pathname + '%7Cutmcmd%3Dreferral%3B%2B__utmv%3D'
-            +cookie+'.-%3B';
-
-    // trigger the tracking
-    img.src = urchinUrl;
-  })("UA-30567117-1","alefeuvre.github.io","gd-bookmarklet.js");
 })();
